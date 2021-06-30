29639990a259b80119b84cb0c689d5f66206e4af
João <joaolages.dev@gmail.com>
2021-06-30 09:26:50 -0300

    Collections

--- StoreDapper.Domain/StoreContext/Entities/Customer.cs
+++ StoreDapper.Domain/StoreContext/Entities/Customer.cs
@@ -1,29 +1,36 @@
 using System.Collections.Generic;
+using System.Linq;
 using StoreDapper.Domain.StoreContext.ValueObjects;
 
 namespace StoreDapper.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses; // private => _
         public Customer(
             Name name,
-            Document document, 
-            Email email, 
-            string phone, 
+            Document document,
+            Email email,
+            string phone,
             string address)
         {
             Name = name;
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
-        public Name Name {get; private set;}
+        public Name Name { get; private set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
--- StoreDapper.Domain/StoreContext/Entities/Order.cs
+++ StoreDapper.Domain/StoreContext/Entities/Order.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using StoreDapper.Domain.StoreContext.Enums;
 
 namespace StoreDapper.Domain.StoreContext.Entities
 {
     public class Order
     {
+        private readonly IList<OrderItem> _items;
+        private readonly IList<Delivery> _deliveries;
         public Order(Customer customer)
         {
             Customer = customer;
             Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 8).ToUpper();
             CreateDate = DateTime.Now;
             Status = EOrderStatus.Created;
-            Items = new List<OrderItem>();
-            Deliveries = new List<Delivery>();
+            _items = new List<OrderItem>();
+            _deliveries = new List<Delivery>();
         }
 
         public Customer Customer { get; private set; }
@@ -22,13 +25,20 @@ namespace StoreDapper.Domain.StoreContext.Entities
         public EOrderStatus Status { get; private set; }
         
         //IReadOnlyCollection => permite que eu não consiga chamar order.Items.Add
-        public IReadOnlyCollection<OrderItem> Items { get; private set; }
-        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items => _items.ToArray();
+        public IReadOnlyCollection<Delivery> Deliveries => _deliveries.ToArray();
 
         public void AddItem(OrderItem item)
         {
-
+            _items.Add(item);
         }
+
+         public void AddDelivery(Delivery delivery)
+        {
+            _deliveries.Add(delivery);
+        }
+
+
         public void Place()
         {
             // Escrever método
