9401f8746376ceeade66e33513a0246ddefd39ce
João <joaolages.dev@gmail.com>
2021-07-01 09:35:54 -0300

    Implementa Notificações

--- StoreDapper.Domain/StoreContext/ValueObjects/Name.cs
+++ StoreDapper.Domain/StoreContext/ValueObjects/Name.cs
@@ -1,11 +1,22 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+
 namespace StoreDapper.Domain.StoreContext.ValueObjects
 {
-    public class Name
+    public class Name : Notifiable
     {
         public Name(string firstName, string lastName)
         {
             FirstName = firstName;
             LastName = lastName;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "FirstName", "O nome deve conter no máximo 40 caracteres")
+                .HasMinLen(FirstName, 3, "LastName", "O sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "LastName", "O sobrenome deve conter no máximo 40 caracteres")
+            );
         }
 
         public string FirstName { get; private set; }
