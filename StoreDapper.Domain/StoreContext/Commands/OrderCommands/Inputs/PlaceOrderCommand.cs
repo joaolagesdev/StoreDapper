7aba9f821a12d9248757073589ffdbd99bc92af6
João <joaolages.dev@gmail.com>
2021-07-05 11:30:33 -0300

    Aplica validações nos comandos

--- StoreDapper.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
+++ StoreDapper.Domain/StoreContext/Commands/OrderCommands/Inputs/PlaceOrderCommand.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Collections.Generic;
+using Flunt.Notifications;
+using Flunt.Validations;
+using StoreDapper.Shared.Commands;
 
 namespace StoreDapper.Domain.StoreContext.Commands.OrderCommands.Inputs
 {
-    public class PlaceOrderCommand
+    public class PlaceOrderCommand : Notifiable, ICommand
     {
+        public PlaceOrderCommand()
+        {
+            OrderItems = new List<OrderItemCommand>();
+        }
         public Guid Customer {get; set;}
         public IList<OrderItemCommand> OrderItems {get; set;}
+         public bool IsValid()
+        {
+            AddNotifications(new Contract()
+                .HasLen(Customer.ToString(), 36, "Customer", "Identificador do Cliente inválido")
+                .IsGreaterThan(OrderItems.Count, 0, "Items", "Nenhum item do pedido foi encontrado")
+            );
+            return IsValid();
+        }
     }
 
     public class OrderItemCommand
