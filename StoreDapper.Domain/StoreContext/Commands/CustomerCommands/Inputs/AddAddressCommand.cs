d41e002ff20b8abb06a59a7b5c13a3ca5be80358
João <joaolages.dev@gmail.com>
2021-07-06 09:53:58 -0300

    Cria interface do serviço para enviar e-mail para o cliente

--- StoreDapper.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommand.cs
+++ StoreDapper.Domain/StoreContext/Commands/CustomerCommands/Inputs/AddAddressCommand.cs
@@ -1,9 +1,11 @@
 using System;
+using Flunt.Notifications;
 using StoreDapper.Domain.StoreContext.Enums;
+using StoreDapper.Shared.Commands;
 
 namespace StoreDapper.Domain.StoreContext.Commands.CustomerCommands.Inputs
 {
-    public class AddAddressCommand
+    public class AddAddressCommand : Notifiable, ICommand
     {
         public Guid Id { get; set; }
         public string Street { get; set; }
@@ -15,5 +17,10 @@ namespace StoreDapper.Domain.StoreContext.Commands.CustomerCommands.Inputs
         public string Country { get; set; }
         public string ZipCode { get; set; }
         public EAddressType Type { get; set; }
+
+        public bool IsValid()
+        {
+            return Valid;
+        }
     }
 }
