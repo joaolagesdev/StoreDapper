e6d0086d2ad470f723dc6f10d6c4f1f494fed6a8
João <joaolages.dev@gmail.com>
2021-07-22 09:54:44 -0300

    Cria DataContext e adiciona ConnectionString

---
+++ StoreDapper.Infra/StoreContext/DataContexts/StoreDataContext.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StoreDapper.Infra.StoreContext.DataContexts
+{
+    public class StoreDataContext : IDisposable
+    {
+         public SqlConnection Connection { get; set; }
+
+        public StoreDataContext()
+        {
+            Connection = new SqlConnection(Settings.ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if(Connection.State != ConnectionState.Closed)
+                Connection.Close();
+        }
+    }
+}
---
+++ StoreDapper.Shared/Settings.cs
@@ -0,0 +1,7 @@
+namespace StoreDapper.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=.\sqlexpress;Database=storedapper;User ID=SQLFRAME;";
+    }
+}
