442fac59c2249f760ade4635ab9612c3e9232aab
João <joaolages.dev@gmail.com>
2021-07-25 22:37:28 -0300

    Utiliza os handlers

--- StoreDapper.Api/Startup.cs
+++ StoreDapper.Api/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StoreDapper.Domain.StoreContext.Handlers;
 using StoreDapper.Domain.StoreContext.Repositories;
 using StoreDapper.Domain.StoreContext.Services;
 using StoreDapper.Infra.StoreContext.DataContexts;
@@ -18,6 +19,7 @@ namespace StoreDapper.Api
             services.AddScoped<StoreDataContext, StoreDataContext>();
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<CustomerHandler, CustomerHandler>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
